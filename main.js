071dc480b3cb299ec882dd113d64f0e8e8b37179
moh-qureshi <h-qureshi95@hotmail.co.uk>
2022-08-30 22:22:07 +0100

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
-console.log("test")
+const carList = document.getElementById("car-list")
+const searchBar = document.getElementById("search-bar")
+
+let allMakes = []
+fetch('https://private-anon-96247447db-carsapi1.apiary-mock.com/manufacturers'
+    )
+    .then(res => res.json())
+    
+    
+    .then(data => {
+        for(let i = 0; i < data.length; i++){
+            allMakes.push(data[i].name)
+            allMakes.sort()
+        }
+        for(let i = 0; i < allMakes.length; i++){
+            const carTextDivs = document.createElement("div")
+            carList.appendChild(carTextDivs).setAttribute("class", "car-makes-list")
+            const carText = document.createElement("p")
+            carTextDivs.appendChild(carText)
+            carText.innerText = allMakes[i].toUpperCase()
+            carList.appendChild(carTextDivs)
+            // const carLogoImg = document.createElement("img")
+            // carLogoImg.src = data[i].img_url
+            // carTextDivs.appendChild(carLogoImg)
+        }
+    })
+
+    fetch(`http://localhost:5001/allMakes`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+
+    searchBar.addEventListener("keyup", (e) =>{
+        const allBrands = carList.querySelectorAll("p")
+        for(let i = 0; i < allBrands.length; i++){
+            filteredMakes = allBrands[i].textContent
+            if(!filteredMakes.includes(searchBar.value)){
+                allBrands[i].style.display="none"
+            } else if(filteredMakes.includes(searchBar.value)){
+                allBrands[i].style.display="initial"
+            }
+        }
+    })
